d0c41a7fbff92ca3c714f057992f49dab1c6e7d3
celilileri <celil.ileri@bil.omu.edu.tr>
2016-09-15 01:38:52 +0300

    Diff Apache Dirs Add

--- domainadd.sh
+++ domainadd.sh
@@ -29,7 +29,7 @@ fi
 
 if [ -e $CONF_FILE ] # Controlling is conf file exits?
 then
-  echo "There is already a $CONF_FILE has generated."
+  echo "There is already a $CONF_FILE exists."
   echo -n "Do you want to change it? (y/N): "
   read ANSWER
   if [[ ( "$ANSWER" != "y" ) && ( "$ANSWER" != "Y" ) && ( "$ANSWER" != "yes" )
