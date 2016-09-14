748de6b6e99fe3f7005b7c5e6ad887d42008a4d3
celilileri <celil.ileri@bil.omu.edu.tr>
2016-09-15 01:31:53 +0300

    Diff Apache Dirs Add

--- domainadd.sh
+++ domainadd.sh
@@ -14,20 +14,39 @@ fi
 
 echo "Creating directory...";
 mkdir /var/www/$DOMAIN;
-CONF_FILE="/etc/httpd/conf.d/$DOMAIN.conf"
-
 echo "Configuring...";
+
+if [ -e "/etc/httpd" ]
+then
+  CONF_FILE="/etc/httpd/conf.d/$DOMAIN.conf"
+elif [ -e "/etc/apache2" ]
+then
+  CONF_FILE="/etc/apache2/sites-available/$DOMAIN.conf"
+else
+  echo "Couldn't find Apache2 configurate directory. Exiting..."
+  exit
+fi
 
 if [ -e $CONF_FILE ] # Controlling is conf file exits?
 then
-  cat > $CONF_FILE <<EOL
+  echo "There is already a $CONF_FILE has generated."
+  echo -n "Do you want to change it? (y/N): "
+  read ANSWER
+  if [[ ( "$ANSWER" != "y" ) && ( "$ANSWER" != "Y" ) && ( "$ANSWER" != "yes" )
+   && ( "$ANSWER" != "Yes" ) && ( "$ANSWER" != "YES" )]]
+  then
+    echo "Aborting..."
+    exit
+  fi
+fi
+
+cat > $CONF_FILE <<EOL
 <VirtualHost *:80>
     ServerName $DOMAIN
     ServerAlias www.$DOMAIN
     DocumentRoot /var/www/$DOMAIN
 </VirtualHost>
 EOL
-fi
 
 echo "Apache Service is reloading..."
 systemctl reload httpd.service
